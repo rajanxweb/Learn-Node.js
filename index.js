75b379afccf7f49d6986dadba52de70cbdbe6c35
Rajan Yadav <rajan.yadav2024@lpu.in>
2025-12-04 07:46:00 +0530

    learn callback, promise, and async await

--- index.js
+++ index.js
@@ -35,3 +35,84 @@ getData(1, ()=>{
     });
 })
 
+promises
+
+const getPromise = ()=>{
+    return new Promise((reject, resolve )=>{
+    console.log("I am a promise");
+    reject("Some error occured");
+})
+}
+
+let promise = getPromise();
+promise.then((res)=>{
+    console.log("success, ", res);
+})
+
+promise.catch((err)=>{
+    console.log("error, ", err);
+})
+
+function asyncFunc1(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("data1");
+            resolve("success");
+        },5000)
+    })
+}
+function asyncFunc2(){
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            console.log("data2");
+            resolve("success");
+        }, 5000);
+    })
+}
+
+// promise chain
+
+console.log("fetching data1.....");
+asyncFunc1().then((res)=>{
+    console.log(res);
+    console.log("fetching data2.....");
+    asyncFunc2().then((res)=>{
+        console.log(res);
+    })
+    
+})
+
+async function hello(){
+    console.log("hell world");
+}
+
+// async await
+
+function getData(dataID){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("data", dataID);
+            resolve("success");
+        }, 2000);
+    });
+}
+
+async function getAllData(){
+    console.log("getting data1...");
+    await getData(1);
+    console.log("getting data2...");
+    await getData(2);
+    console.log("getting data3...");
+    await getData(3);
+}
+
+// IIFE functions
+
+(async function (){
+    console.log("getting data1...");
+    await getData(1);
+    console.log("getting data2...");
+    await getData(2);
+    console.log("getting data3...");
+    await getData(3);
+})();
